2891d1e812a9afed6dabfb61ae04343754a30a0c
youbaidjis <youba.etud@gmail.com>
2022-03-24 23:48:19 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+     return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+/*function wrapAdjective(singlePar="special"){
+        if (singlePar = '*'){
+            return function(emphatic){
+                return `You are ${emphatic}`;
+            }
+        }if (singlePar = '||'){
+            return function(emphatic){
+                return `You are ${emphatic}`;
+            }
+        }
+}*/
+
+
+
+
+function wrapAdjective(singlePar) {
+    const base = "You are";
+    return function (visualFlair) {
+      return `${base} ${singlePar}${visualFlair}${singlePar}!`;
+    };
+  }
